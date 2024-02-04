81dc645ac47f7b46d4115f4fd38ee603d212cf38
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-02-04 23:39:17 +0300

    Обработка исключений

--- src/main/java/com/example/producingwebservice/services/rest/DoctorService.java
+++ src/main/java/com/example/producingwebservice/services/rest/DoctorService.java
@@ -1,8 +1,12 @@
 package com.example.producingwebservice.services.rest;
 
 import com.example.producingwebservice.entity.Doctor;
+import com.example.producingwebservice.exception.NullUserException;
+import com.example.producingwebservice.exception.NotFoundException;
+import com.example.producingwebservice.exception.UsersNotFoundException;
 import com.example.producingwebservice.repository.DoctorRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,27 +14,56 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DoctorService {
     private final DoctorRepository doctorRepository;
 
     public Doctor createDoctor(String fullNme) {
-        Doctor doctor = new Doctor(fullNme);
-        doctorRepository.save(doctor);
-        return doctor;
+        if (!fullNme.isEmpty()) {
+            Doctor doctor = new Doctor(fullNme);
+            doctorRepository.save(doctor);
+
+            log.info("Успешное сохранение врача в БД");
+            return doctor;
+        } else {
+            log.error("Ошибка сохранение врача в БД!!! Передано пустое поле!");
+            throw new NullUserException("Имя не может быть пустым! Назначьте ФИО врача", "NUFN-0099");
+        }
     }
 
     public void deleteDoctorByID(Long id) {
-        doctorRepository.deleteById(id);
+        Optional<Doctor> doctor = doctorRepository.findById(id);
+        if (doctor.isPresent()) {
+            doctorRepository.deleteById(id);
+
+            log.info("Успешное удаление врача с id {}", id);
+        } else {
+            log.error("Ошибка удаления врача! Не найден врач с id {}", id);
+            throw new NotFoundException("Врач с ID " + id + " не найден!", "NFDID-0098");
+        }
     }
 
     public Optional<Doctor> updateDoctor(Long id, String updatedFullName) {
         Optional<Doctor> doctor = doctorRepository.findById(id);
-        doctor.get().setFullName(updatedFullName);
-        doctorRepository.save(doctor.get());
-        return doctor;
+        if (doctor.isPresent()) {
+            doctor.get().setFullName(updatedFullName);
+            doctorRepository.save(doctor.get());
+
+            log.info("Успешное обновление информации!");
+            return doctor;
+        } else {
+            log.error("Ошибка обновления! Не найден врач с id {}", id);
+            throw new NotFoundException("Врач с ID " + id + " не найден!", "NFDID-0097");
+        }
     }
 
     public List<Doctor> getAllDoctors() {
-        return doctorRepository.findAll();
+        if (!doctorRepository.findAll().isEmpty()) {
+            log.info("Успешное получение всех пользователей!");
+            return doctorRepository.findAll();
+        } else {
+            log.error("Ошибка получения пользователей!");
+            throw new UsersNotFoundException("Список врачей пуст!", "NFDL-0096");
+        }
     }
 }
--- src/main/java/com/example/producingwebservice/services/rest/PatientService.java
+++ src/main/java/com/example/producingwebservice/services/rest/PatientService.java
@@ -3,10 +3,14 @@ package com.example.producingwebservice.services.rest;
 import com.example.producingwebservice.entity.Doctor;
 import com.example.producingwebservice.entity.Patient;
 import com.example.producingwebservice.entity.Ticket;
+import com.example.producingwebservice.exception.NotFoundException;
+import com.example.producingwebservice.exception.NullUserException;
+import com.example.producingwebservice.exception.UsersNotFoundException;
 import com.example.producingwebservice.repository.DoctorRepository;
 import com.example.producingwebservice.repository.PatientRepository;
 import com.example.producingwebservice.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -15,46 +19,110 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PatientService {
     private final TicketRepository ticketRepository;
     private final PatientRepository patientRepository;
     private final DoctorRepository doctorRepository;
 
-    public void appointTicket(Long ticketId, Long patientId, Long doctorId) {
-
-        Optional<Ticket> optionalTicket = ticketRepository.findById(ticketId);
-        Optional<Patient> optionalPatient = patientRepository.findById(patientId);
-        Optional<Doctor> optionalDoctor = doctorRepository.findById(doctorId);
-
-        if (optionalTicket.isPresent() && optionalPatient.isPresent() && optionalDoctor.isPresent()) {
-            Ticket ticket = optionalTicket.get();
-            Patient patient = optionalPatient.get();
-            Doctor doctor = optionalDoctor.get();
-
-            ticket.setPatient(patient);
-            ticket.setDoctor(doctor);
-            ticketRepository.save(ticket);
-        } else {
-            // Обработка случая, когда ticketId или patientId не найдены
-        }
-    }
+//    public void appointTicket(Long ticketId, Long patientId, Long doctorId) {
+//        Optional<Ticket> optionalTicket = ticketRepository.findById(ticketId);
+//        Optional<Patient> optionalPatient = patientRepository.findById(patientId);
+//        Optional<Doctor> optionalDoctor = doctorRepository.findById(doctorId);
+//
+//        if (optionalTicket.isPresent() && optionalPatient.isPresent() && optionalDoctor.isPresent()) {
+//            Ticket ticket = optionalTicket.get();
+//            Patient patient = optionalPatient.get();
+//            Doctor doctor = optionalDoctor.get();
+//
+//            ticket.setPatient(patient);
+//            ticket.setDoctor(doctor);
+//            ticketRepository.save(ticket);
+//            log.info("Успешное обновление информации!");
+//        } else {
+//            if (optionalTicket.isEmpty()) {
+//                log.error("Ошибка обновления! Не найден талон с id {}", ticketId);
+//                throw new NotFoundException("Билет с ID " + ticketId + " не найден!", "TNFID-0089");
+//            }
+//            if (optionalPatient.isEmpty()) {
+//                log.error("Ошибка обновления! Не найден пациент с id {}", patientId);
+//                throw new NotFoundException("Пациент с ID " + patientId + " не найден!", "PNFID-0088");
+//            }
+//            if (optionalDoctor.isEmpty()) {
+//                log.error("Ошибка добавления! Не найден врач с id {}", doctorId);
+//                throw new NotFoundException("Доктор с ID " + " не найден!", "DNFID-0087");
+//            }
+//        }
+//    }
+//
+//    public void appointTicketDoctor(Long ticketId, Long doctorId) {
+//        Optional<Ticket> optionalTicket = ticketRepository.findById(ticketId);
+//        Optional<Doctor> optionalDoctor = doctorRepository.findById(doctorId);
+//
+//        if (optionalTicket.isPresent() && optionalDoctor.isPresent()) {
+//            Ticket ticket = optionalTicket.get();
+//            Doctor doctor = optionalDoctor.get();
+//
+//            ticket.setDoctor(doctor);
+//            ticketRepository.save(ticket);
+//            log.info("Успешное обновление информации!");
+//        } else {
+//            if (optionalTicket.isEmpty()) {
+//                log.error("Ошибка обновления! Не найден талон с id {}", ticketId);
+//                throw new NotFoundException("Билет с ID " + ticketId + " не найден!", "TNFID-0089");
+//            }
+//            if (optionalDoctor.isEmpty()) {
+//                log.error("Ошибка добавления! Не найден врач с id {}", doctorId);
+//                throw new NotFoundException("Доктор с ID " + " не найден!", "DNFID-0087");
+//            }
+//        }
+//    }
 
     public Patient createPatient(String fullNme, Date birthDate) {
         Patient createPatient = new Patient(fullNme, birthDate);
-        patientRepository.save(createPatient);
+        if (!fullNme.isEmpty() && !birthDate.equals(null)) {
+
+            patientRepository.save(createPatient);
+            log.info("Успешное сохранение пациента в БД");
+        } else {
+            if (fullNme.isEmpty()) {
+                log.error("Ошибка сохранение пациента в БД!!! Передано пустое поле в ФИО!");
+                throw new NullUserException("Имя не может быть пустым! Назначьте ФИО пациента", "EFN-0079");
+            }
+            if (birthDate.equals(null)) {
+                log.error("Ошибка сохранение пациента в БД!!! Передано пустое поле в дату рождения!");
+                throw new NullUserException("Дата рождения не может быть пустой! Назначьте дату", "EDB-0078");
+            }
+        }
         return createPatient;
     }
 
-    public void delete(Long id) {
-        patientRepository.deleteById(id);
+    public void deletePatientById(Long id) {
+        Optional<Patient> patient = patientRepository.findById(id);
+        if (patient.isPresent()) {
+            patientRepository.deleteById(id);
+        } else {
+            throw new NotFoundException("Пациент с ID " + id + " не найден!", "NFDID-0078");
+        }
     }
 
-    public Optional<Patient> update(Long id, Date updatedBirthDate, String updatedFullName) {
+    public Optional<Patient> updatePatient(Long id, Date updatedBirthDate, String updatedFullName) {
         Optional<Patient> patient = patientRepository.findById(id);
-        patient.get().setBirthDate(updatedBirthDate);
-        patient.get().setFullName(updatedFullName);
-        patientRepository.save(patient.get());
-        return patient;
+        if (patient.isPresent()) {
+            patient.get().setBirthDate(updatedBirthDate);
+            patient.get().setFullName(updatedFullName);
+            patientRepository.save(patient.get());
+            return patient;
+        } else {
+            throw new NotFoundException("Пациент с ID " + id + " не найден!", "NFDID-0077");
+        }
     }
-    public List<Patient> getAllPatients() {return patientRepository.findAll();}
+
+    public List<Patient> getAllPatients() {
+        if (!patientRepository.findAll().isEmpty()) {
+            return patientRepository.findAll();
+        } else {
+            throw new UsersNotFoundException("Список пациентов пуст!", "NFDL-0076");
+        }
+    }
 }
